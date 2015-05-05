9ace09ccb7d2f886c9cd884c64154d87efb09e21
Graham Gilchrist <mail@grahamgilchrist.com>
2015-05-06 00:19:13 +0100

    Setup exports correctly

--- git-deploy.js
+++ git-deploy.js
@@ -1,36 +1,33 @@
-module.exports = function () {
+/*
+  @method gitPull
+  Takes a flightplan instance and transport
+  @param remote {Object} Flightplan transport instance
+  @param webRoot {string} path to run git pull on the remote server
+   e.g. /var/www/project
+*/
+var gitPull = function (remote, webRoot) {
+  // git pull
+  remote.with('cd ' + webRoot, function() {
+    remote.exec('git pull');
+  });
+};
 
-  /*
-    @method gitPull
-    Takes a flightplan instance and transport
-    @param remote {Object} Flightplan transport instance
-    @param webRoot {string} path to run git pull on the remote server
-     e.g. /var/www/project
-  */
-  var gitPull = function (remote, webRoot) {
-    // git pull
-    remote.with('cd ' + webRoot, function() {
-      remote.exec('git pull');
-    });
-  };
-  
-  /*
-    @method addToPlan
-    Takes a flightplan instance and adds a task called gitDeploy
-    @param plan {Object} Flightplan instance
-    @param plan.runtime.options.webRoot {String} Remote path to run git pull
-  */
-  var addToPlan = function (plan) {
-    // Plan to git deploy via ssh remote
-    var webRoot = plan.runtime.options.webRoot;
+/*
+  @method addToPlan
+  Takes a flightplan instance and adds a task called gitDeploy
+  @param plan {Object} Flightplan instance
+  @param plan.runtime.options.webRoot {String} Remote path to run git pull
+*/
+var addToPlan = function (plan) {
+  // Plan to git deploy via ssh remote
+  var webRoot = plan.runtime.options.webRoot;
 
-    plan.remote('gitDeploy', function(remote) {
-      remoteGitDeploy(remote, webRoot);
-    });
-  };
+  plan.remote('gitDeploy', function(remote) {
+    remoteGitDeploy(remote, webRoot);
+  });
+};
 
-  return {
-    addToPlan: addToPlan,
-    remoteGitDeploy: remoteGitDeploy
-  };
-}
+module.exports = {
+  addToPlan: addToPlan,
+  remoteGitDeploy: remoteGitDeploy
+};
